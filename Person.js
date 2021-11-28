33a37c8857a681f82ec6c5df366514900a40f7c7
shubhashsharma0 <90338107+shubhashsharma0@users.noreply.github.com>
2021-11-28 09:53:08 +0530

    Update Person.js

--- Person.js
+++ Person.js
@@ -7,14 +7,15 @@ this.OfficeAddress=OfficeAddress;
 var Person1=new Person('Ravi','kumar','Patna');
 var Person2=new Person('Harsh','anand','Haryana');
 var Person3=new Person('Keshav','nanda','Delhi');
-console.log(Person1.FirstName);
-console.log(Person1.LastName);
-console.log(Person1.OfficeAddress);
-
-console.log(Person2.FirstName);
-console.log(Person2.LastName);
-console.log(Person2.OfficeAddress);
-
-console.log(Person3.FirstName);
-console.log(Person3.LastName);
-console.log(Person3.OfficeAddress);
+console.log("Person 1 Details");
+console.log("FirstName: "+Person1.FirstName);
+console.log("LastName: "+Person1.LastName);
+console.log("OfficeAddress: "+Person1.OfficeAddress);
+console.log("\nPerson 2 Details");
+console.log("FirstName: "+Person2.FirstName);
+console.log("LastName: "+Person2.LastName);
+console.log("OfficeAddress: "+Person2.OfficeAddress);
+console.log("\nPerson 3 Details");
+console.log("FirstName: "+Person3.FirstName);
+console.log("LastName: "+Person3.LastName);
+console.log("OfficeAddress: "+Person3.OfficeAddress);
